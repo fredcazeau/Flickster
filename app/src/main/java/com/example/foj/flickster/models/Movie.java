a414960bac97ada2b95c8674e7b7bdb59d662c8c
fredcazeau <fredykito@yahoo.com>
2016-07-19 19:08:00 -0400

    Flickster App Movie

---
+++ app/src/main/java/com/example/foj/flickster/models/Movie.java
@@ -0,0 +1,51 @@
+package com.example.foj.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by FOJ on 7/17/2016.
+ */
+public class Movie {
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    String posterPath;
+    String originalTitle;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.originalTitle = jsonObject.getString("original_title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array) {
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for (int x = 0; x < array.length(); x++) {
+            try {
+                results.add(new Movie(array.getJSONObject(x)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return results;
+    }
+
+    }
+
